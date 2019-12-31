b6f41199a0a0636c3db589bbe4bd5e0d00b8958e
Ilham Dwi Nugraha <ilhamdn99@gmail.com>
2019-12-31 16:37:30 +0700

    menambah objek member

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,13 @@
 package app;
 import app.data.Buku;
+import app.data.Member;
 
 import java.util.HashMap;
 
 public class App{
     public static void main(String[] args) {
+
+        // database buku
 
         HashMap<String, Buku> lemariBuku = new HashMap<String, Buku>();
 
@@ -17,9 +20,33 @@ public class App{
             
             String judul = lemariBuku.get(key).getJudulBuku();
             String penulis = lemariBuku.get(key).getPenulis();
+            String jumlah = lemariBuku.get(key).getJumlahHalaman();
             System.out.println("Nomor       :   "+no++);
             System.out.println("Judul Buku  :   "+judul);
             System.out.println("Penulis     :   "+penulis);
+            System.out.println("Volume      :   "+jumlah);
+            System.out.println();
+
+        }
+
+        // database member
+
+        HashMap<String, Member> daftarMember = new HashMap<String, Member>();
+
+        daftarMember.put("id-001", new Member("id-001","Sumireko Sanshokuin","Malromarc District","07771245299"));
+        daftarMember.put("id-002", new Member("id-002","Mikasa Ackerman","Shigansina District","07709471620"));
+        daftarMember.put("id-003", new Member("id-002","Izumi Sagiri","Tegal District","07777444612"));
+
+        int noM = 1;
+        for (String key : daftarMember.keySet()){
+            
+            String nama = daftarMember.get(key).getNamaMember();
+            String alamat = daftarMember.get(key).getAlamatMember();
+            String nohp = daftarMember.get(key).getNoHp();
+            System.out.println("Nomor       :   "+noM++);
+            System.out.println("Nama Member :   "+nama);
+            System.out.println("Alamat      :   "+alamat);
+            System.out.println("nomor Hp    :   "+nohp);
             System.out.println();
 
         }
---
+++ src/app/data/Member.java
@@ -0,0 +1,57 @@
+package app.data;
+
+public class Member{
+    private String idMember;
+    private String namaMember;
+    private String alamatMember;
+    private String noHp;
+
+    public Member(String idMember, String namaMember, String alamatMember, String noHp) {
+        this.idMember = idMember;
+        this.namaMember = namaMember;
+        this.alamatMember = alamatMember;
+        this.noHp = noHp;
+    }
+
+    public void meminjamBuku(){
+        System.out.println(this.idMember+" Telah meminjam buku");
+    }
+
+    public void mengembalikanBuku(){
+        System.out.println(this.idMember+" Telah mengembalikan buku");
+    }
+
+    public String getIdMember() {
+        return idMember;
+    }
+
+    public void setIdMember(String idMember) {
+        this.idMember = idMember;
+    }
+
+    public String getNamaMember() {
+        return namaMember;
+    }
+
+    public void setNamaMember(String namaMember) {
+        this.namaMember = namaMember;
+    }
+
+    public String getAlamatMember() {
+        return alamatMember;
+    }
+
+    public void setAlamatMember(String alamatMember) {
+        this.alamatMember = alamatMember;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+
+    
+}
